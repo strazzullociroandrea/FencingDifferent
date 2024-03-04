543177fefd13c0f4d739f68c861d109f7db6d7f9
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-03-04 23:51:43 +0100

    ok

--- public/utenteSenzaLogin/src/index.js
+++
@@ -1,9 +0,0 @@
-//Import moduli
-import { renderTornei } from "./render.js";
-
-/**
- * Al caricamento della pagina viene fatto il render dei tornei
- */
-window.onload = () => {
-  renderTornei();
-};
