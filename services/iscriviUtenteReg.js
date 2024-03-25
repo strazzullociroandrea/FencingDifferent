32179761fa107b3eec9f66b5144c2cc77664b23c
MattiaGrazianoPedicini <pedicinimattiagraziano@itis-molinari.eu>
2024-03-25 13:27:24 +0100

    ciro modifica servizio

--- services/iscriviUtenteReg.js
+++ services/iscriviUtenteReg.js
@@ -5,24 +5,47 @@ const iscriviUtenteReg = async (nomeTorneo, data, user) => {
   try {
     const conf = JSON.parse(fs.readFileSync("conf.json"));
     const connection = await mysql.createConnection(conf);
-
-    const [rows, fields] = await connection.execute(
-      "SELECT * FROM atleta WHERE atleta.emailUtente = '" + user + "'"
-    );
-    const [rows2, fields2] = await connection.execute(
-      "INSERT INTO partecipare VALUES (" +
-        rows[0].codiceFis +
-        ", '" +
-        nomeTorneo +
-        "', '" +
-        data +
-        "', '','iniziale', '')"
-    );
-    await connection.end();
-    if (rows.length > 0) {
-      return rows;
+    const verificaSql =
+      "SELECT * FROM partecipare WHERE codiceFisAtleta=(" +
+      "SELECT codiceFis FROM atleta WHERE emailUtente='" +
+      user +
+      "') AND nomeTorneo = '" +
+      nomeTorneo +
+      "' AND dataTorneo='" +
+      data +
+      "'";
+    const [rows, fields] = await connection.execute(verificaSql);
+    if (rows > 0) {
+      await connection.end();
+      return { response: "Utente già registrato" };
     } else {
-      return [];
+      let codiceFis =
+        "SELECT codiceFis FROM atleta WHERE emailUtente='" + user + "'";
+      const [user, fields] = await connection.execute(verificaSql);
+      codiceFis = user[0]["codiceFis"];
+      let idPrimoAssalto =
+        "SELECT id FROM fase WHERE nomeTorneo='" +
+        nomeTorneo +
+        "' AND dataTorneo='" +
+        data +
+        "'";
+      const [assalto, fieldss] = await connection.execute(idPrimoAssalto);
+      idPrimoAssalto = assalto[0]["id"];
+
+      const sqlAdd =
+        "INSERT INTO partecipare(codiceFisAtleta, nomeTorneo, dataTorneo, idAssalto, idFase, punteggio) VALUES('" +
+        codiceFis +
+        "', '" +
+        nomeTorneo +
+        "','" +
+        data +
+        "','-1','" +
+        idPrimoAssalto +
+        "','-1')";
+
+      const [fne, fs] = await connection.execute(sqlAdd);
+      await connection.end();
+      return { response: "Utente registrato con successo" };
     }
   } catch (error) {
     throw new Error(error.message);
